40c8c0d28768c78b22c4b3eb5052c153e3e7aeff
Felipe Gama Danta <epilefgama2@gmail.com>
2022-02-18 17:58:30 -0300

    requisito 9 e 10

---
+++ src/components/MissionCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid={ name }>{name}</p>
+        <p data-testid={ year }>{year}</p>
+        <p data-testid={ country }>{country}</p>
+        <p data-testid={ destination }>{destination}</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
+
+export default MissionCard;
