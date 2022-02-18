17c4e3ed6c0c0d262e3159db0241029eef8918c3
Felipe Gama Danta <epilefgama2@gmail.com>
2022-02-18 16:58:25 -0300

    requisito 6, 50%

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage} = this.props;
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
         <p>{planetName}</p>
